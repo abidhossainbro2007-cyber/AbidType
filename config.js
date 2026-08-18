76f937d7f99895a2dfe43a1ea3f13e25f5f53a11
abidhossainbro2007-cyber <abidhossainbro2007@gmail.com>
2026-08-18 18:12:51 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ABIDTYPE CONFIG
 // 1) Replace the text inside the quotes with your Supabase PUBLISHABLE key.
 // 2) Do NOT use the sb_secret key here.
-window.ABIDTYPE_SUPABASE_URL = "https://balrfqlbvddapimjiow.supabase.co";
+window.ABIDTYPE_SUPABASE_URL = "https://balrfqlvbvddapimjiow.supabase.co";
 window.ABIDTYPE_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_4pjhmu3rJN8PQjN-bWmaVQ__naHXqNb";
